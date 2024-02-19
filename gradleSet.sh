7327d7cbbf6e93dce2ed6d7f71c585e361ce3dab
Morales Ortega, Carlos Alberto <carlos.mor.ort@gmail.com>
2024-02-18 22:15:57 -0600

    Adding gradleSet file

---
+++ gradleSet.sh
@@ -0,0 +1,51 @@
+    local PARAM_GRADLE=$1
+    echo "desired gradle version $PARAM_GRADLE"
+        local TEMP=$(gradle -v 2>/dev/null | grep Gradle | grep -oE ' [0-9\.]+' | grep -oE '[0-9\.]+' || true)
+    if [ ! -z "$TEMP" ]; then
+        until [ ! -z "$GRADLE_VERSION" ]; do
+            for FORMULA in $(brew list | grep gradle ); do
+                if [ "$FORMULA" = "gradle" ]; then
+                    local VER_FORMULA=8
+                else
+                    local VER_FORMULA=$(echo $FORMULA | grep -oE "@[0-9\.]+"| grep -oE "[0-9]+(\.[0-9]+)?")
+                fi
+                IS_GRADLE_VER=$(echo $TEMP | grep -cE "^$VER_FORMULA$" || true)
+                if [ "$IS_GRADLE_VER" -gt 0 ]; then
+                    local GRADLE_VERSION=$TEMP
+                    break;
+                fi
+            done
+            local TEMP=${TEMP%?}
+        done
+    fi
+    echo "current gradle version: $GRADLE_VERSION"
+
+     if [ -z "$PARAM_GRADLE" ]; then
+        echo "gradle version not set"
+        return 1
+    fi
+    local IS_GRADLE_SET=$(echo $GRADLE_VERSION | grep -cE "^$PARAM_GRADLE$" || true)
+
+    if [ "$IS_GRADLE_SET" -gt 0 ]; then
+        echo "gradle version already set"
+    else
+
+        declare -A GRADLE_MAP
+
+        GRADLE_MAP["$(brew info gradle | grep -oE 'stable [0-9]*\.' | grep -oE ' [0-9]*\.' | grep -oE '[0-9]*')"]="gradle"
+        for FORMULA in $(brew list | grep gradle@ ); do
+            GRADLE_MAP["$(echo $FORMULA | grep -oE "@[0-9\.]+"| grep -oE "[0-9]+(\.[0-9]+)?")"]=$FORMULA
+        done
+
+        #for key in "${(@k)GRADLE_MAP}"; do
+        #   echo "$key:${GRADLE_MAP[$key]}"
+        #done
+        if [ ! -z "$GRADLE_VERSION" ]; then
+            echo "removing current gradle version ${GRADLE_MAP["$GRADLE_VERSION"]}"
+            brew unlink ${GRADLE_MAP["$GRADLE_VERSION"]}
+        fi
+
+        brew link ${GRADLE_MAP["$PARAM_GRADLE"]}
+        local GRADLE_VERSION=$(gradle -v 2>/dev/null | grep Gradle | grep -oE ' [0-9\.]+' | grep -oE '[0-9\.]+' || true)
+        echo "Now using gradle version: $GRADLE_VERSION"
+    fi
--- setJavaGradle.sh
+++ setJavaGradle.sh
@@ -1,84 +1,16 @@
 set_args() {
+    java -version
     #export JAVA_HOME=`/usr/libexec/java_home -v 17`
-    local PARAM_GRADLE=$1
-    echo "desired gradle version $PARAM_GRADLE"
+    source /Users/carlosmorales/IdeaProjects/stuff/gradleSet.sh $1
     local PARAM_JAVA=$2
-    local PARAM_JAVA=17
+    #local PARAM_JAVA=17
     echo "desired java version $PARAM_JAVA"
-    local TEMP=$(gradle -v 2>/dev/null | grep Gradle | grep -oE ' [0-9\.]+' | grep -oE '[0-9\.]+' || true)
-    if [ ! -z "$TEMP" ]; then
-        until [ ! -z "$GRADLE_VERSION" ]; do
-            for FORMULA in $(brew list | grep gradle ); do
-                if [ "$FORMULA" = "gradle" ]; then
-                    local VER_FORMULA=8
-                else
-                    local VER_FORMULA=$(echo $FORMULA | grep -oE "@[0-9\.]+"| grep -oE "[0-9]+(\.[0-9]+)?")
-                fi
-                IS_GRADLE_VER=$(echo $TEMP | grep -cE "^$VER_FORMULA$" || true)
-                if [ "$IS_GRADLE_VER" -gt 0 ]; then
-                    local GRADLE_VERSION=$TEMP
-                    break;
-                fi
-            done
-            local TEMP=${TEMP%?}
-        done
-    fi
-    echo "current gradle version: $GRADLE_VERSION"
     local JAVA_VERSION=$(java -version  2>&1 | grep -oE '"[0-9\.\_]+\"' | grep -oE '[0-9\.\_]+' )
     echo "current java version: $JAVA_VERSION"
-    if [ -z "$PARAM_GRADLE" ] || [ -z "$PARAM_JAVA" ]; then
-        echo "args not set"
-        return 1
-    fi
-    local IS_GRADLE_SET=$(echo $GRADLE_VERSION | grep -cE "^$PARAM_GRADLE$" || true)
-
-    if [ "$IS_GRADLE_SET" -gt 0 ]; then
-        echo "gradle version already set"
-    else
-
-        declare -A GRADLE_MAP
-
-        GRADLE_MAP["$(brew info gradle | grep -oE 'stable [0-9]*\.' | grep -oE ' [0-9]*\.' | grep -oE '[0-9]*')"]="gradle"
-        for FORMULA in $(brew list | grep gradle@ ); do
-            GRADLE_MAP["$(echo $FORMULA | grep -oE "@[0-9\.]+"| grep -oE "[0-9]+(\.[0-9]+)?")"]=$FORMULA
-        done
-
-        #for key in "${(@k)GRADLE_MAP}"; do
-        #   echo "$key:${GRADLE_MAP[$key]}"
-        #done
-        if [ ! -z "$GRADLE_VERSION" ]; then
-            echo "removing current gradle version ${GRADLE_MAP["$GRADLE_VERSION"]}"
-            brew unlink ${GRADLE_MAP["$GRADLE_VERSION"]}
-        fi
-
-        brew link ${GRADLE_MAP["$PARAM_GRADLE"]}
-        local GRADLE_VERSION=$(gradle -v 2>/dev/null | grep Gradle | grep -oE ' [0-9\.]+' | grep -oE '[0-9\.]+' || true)
-        echo "Now using gradle version: $GRADLE_VERSION"
-    fi
-
+   
     text=$(/usr/libexec/java_home -V 2>&1)
-local VERSIONS_JAVA=$(
-python3 - <<END
-import re
-
-text = """$text"""
-my_dictionary = {}
-pattern = r"^.+\s\-\s\"[\w\s\.]+\"\s/.+$"
-matches = re.findall(pattern, text, re.MULTILINE)
-result = ''
-for match in matches:
-    ver_pattern = r"(?<=\s)/.+$"
-    version = re.search(ver_pattern,match).group()
-    key_pattern = r"^\s+[0-9\._]+"
-    key = re.search(key_pattern,match).group()
-    key_pattern = r"[0-9\._]+"
-    key = re.search(key_pattern,match).group()
-    print(key+':'+version.replace("\"",""))
-    ##my_dictionary[key] = version.replace("\"","")
-##print(my_dictionary)
-END
-)
-
+    VERSIONS_JAVA=$(echo "$text" | /Users/carlosmorales/IdeaProjects/stuff/parse_java_versions.py)
+    echo "$VERSIONS_JAVA"
     local VERSIONS_JAVA=("${(f)VERSIONS_JAVA}")
     declare -A JAVA_MAP
     local JAVA_KEYS=()
