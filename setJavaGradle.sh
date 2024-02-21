3d8964a02416460af21d467e81ea0b097a1aa2ca
Morales Ortega, Carlos Alberto <carlos.mor.ort@gmail.com>
2024-02-21 13:38:29 -0600

    Added gcp-check

--- setJavaGradle.sh
+++ setJavaGradle.sh
@@ -2,3 +2,14 @@ set_args() {
     source /Users/carlosmorales/IdeaProjects/stuff/gradleSet.sh $1
     source /Users/carlosmorales/IdeaProjects/stuff/javaSet.sh $2
 }
+
+if [ ! -z "$VER_GRADLE" ] && [ ! -z "$VER_JAVA" ]; then
+    set_args $VER_GRADLE $VER_JAVA
+fi
+
+if [ "$CHECK_GCLOUD" = "true" ]; then
+    $IS_LOGGED=$(gcloud auth print-access-token | grep -cE 'Reauthentication required' || true)
+    if [ "$IS_LOGGED" -eq 0 ]; then
+        gcloud auth application-default login
+    fi
+fi
