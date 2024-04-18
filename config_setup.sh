d36757de832b3c08f4dc4b38c697efecd580300f
Morales Ortega, Carlos Alberto <carlos.mor.ort@gmail.com>
2024-04-18 11:02:36 -0600

    Changing config to function

--- config_setup.sh
+++ config_setup.sh
@@ -1,21 +1,24 @@
-WS_TEMP="./workspace_temp"
-if [ ! -z "$VER_GRADLE" ]; then
-    source /Users/carlosmorales/IdeaProjects/stuff/gradleSet.sh
-    set_gradle $VER_GRADLE
-fi
+setup_env(){
+    WS_TEMP="./workspace_temp"
+    if [ ! -z "$VER_GRADLE" ]; then
+        source /Users/carlosmorales/IdeaProjects/stuff/gradleSet.sh
+        set_gradle $VER_GRADLE
+    fi
 
-if [ ! -z "$VER_JAVA" ]; then
-    source /Users/carlosmorales/IdeaProjects/stuff/javaSet.sh
-    set_java $VER_JAVA
-fi
+    if [ ! -z "$VER_JAVA" ]; then
+        source /Users/carlosmorales/IdeaProjects/stuff/javaSet.sh
+        set_java $VER_JAVA
+    fi
 
-if [ "$CHECK_GCLOUD" = "true" ]; then
-    source /Users/carlosmorales/IdeaProjects/stuff/gcloudSet.sh
-    setCloud
-fi
+    if [ "$CHECK_GCLOUD" = "true" ]; then
+        source /Users/carlosmorales/IdeaProjects/stuff/gcloudSet.sh
+        setCloud
+    fi
 
-if [ "$UNTRACK_PROPS" = "true" ]; then
-    source /Users/carlosmorales/IdeaProjects/stuff/untrackGradleProps.sh
-    untrackProperties
-fi
-source /Users/carlosmorales/IdeaProjects/stuff/branchDetails.sh
+    if [ "$UNTRACK_PROPS" = "true" ]; then
+        source /Users/carlosmorales/IdeaProjects/stuff/untrackGradleProps.sh
+        untrackProperties
+    fi
+    source /Users/carlosmorales/IdeaProjects/stuff/branchDetails.sh
+}
+echo "use the setup_env to start working on a repository"
