425811fadb2e67f5ac0666b5b7155c624a4ec268
Morales Ortega, Carlos Alberto <carlos.mor.ort@gmail.com>
2024-11-24 23:29:42 -0600

    fixing schema tool

--- py/config_environment/graphql_schema.py
+++ py/config_environment/graphql_schema.py
@@ -1,7 +1,8 @@
 """ Creates a GraphQL schema file from a given schema path. """
 
+import json
 import os
-from graphql import build_schema, GraphQLSchema, print_schema
+from graphql import build_schema, GraphQLSchema, print_schema, introspection_from_schema
 from py.util.formatting import ws_success
 from py.util.util import run_operation
 
@@ -28,12 +29,23 @@ def create_schema(schema_path: str, output_file: str) -> None:
     # Build schema
     schema: GraphQLSchema = build_schema(schema_content)
 
+    introspection_dict = introspection_from_schema(schema)
+
     # Serialize schema to JSON
     schema_str: str = print_schema(schema)
 
+    output_json = f"{output_file}.json"
+    output_graphql = f"{output_file}.graphql"
+
     # Write to schema.json
-    with open(output_file, "w", encoding="utf-8") as json_file:
+    with open(output_json, "w", encoding="utf-8") as json_file:
+        json.dump(introspection_dict, json_file, indent=2)
+    
+    with open(output_graphql, "w", encoding="utf-8") as json_file:
         json_file.write(schema_str)
 
-    run_operation(f"code {output_file}", "Opening GraphQL schema")
-    ws_success(f"GraphQL schema file has been generated in {output_file}.")
+    run_operation(f"code {output_graphql}", "Opening GraphQL schema")
+    run_operation(f"code {output_json}", "Opening GraphQL schema JSON")
+
+    ws_success(f"GraphQL schema file has been generated in {output_graphql}.")
+    ws_success(f"GraphQL schema JSON file has been generated in {output_json}.")
